8646e70b69e4a8e684403055352ad14b3d08f95a
zjdavid <zjdavid.2003@gmail.com>
2020-07-23 07:59:21 +0800

    PUT Test

--- app.py
+++ app.py
@@ -2,6 +2,15 @@ from typing import Optional
 
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Optional[bool] = None
+
+
 app = FastAPI()
 
 
@@ -13,3 +22,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
