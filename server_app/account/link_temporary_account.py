6979363437e1b7dbbdb26c86399e2522c080bf52
zjdavid <zjdavid.2003@gmail.com>
2020-08-22 10:15:41 +0800

    完成link account

--- server_app/account/link_temporary_account.py
+++ server_app/account/link_temporary_account.py
@@ -1,10 +1,7 @@
 from flask import request, jsonify, g
 import datetime
-from server_app.auth_tools import is_username_exist, get_user_with, sign, verify_otp, is_email_exist, generate_user_dict
-import bcrypt
-from config import Config
+from server_app.auth_tools import get_user_with, generate_user_dict
 from data.model import *
-from server_app.email_tools import is_valid_email
 from . import account_blueprint
 from server_app.auth_tools import login_required
 
@@ -25,13 +22,11 @@ def link_account():
         }
 
     @apiSuccess (回参) {String} msg   为"Successful!"
-    @apiSuccess (回参) {String} jwt   token，应当在后续请求中被附在 auth 栏
     @apiSuccess (回参) {String} user  user信息，具体见样例
     @apiSuccessExample {json} 成功样例
         HTTP/1.1 200 OK
         {
             "msg": "Successful!",
-            "jwt": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
             "user": {
                 "username": "foo",
                 "nickname": "bar",
@@ -73,39 +68,28 @@ def link_account():
         return jsonify({"msg": "User does not exist"}), 409
 
     qq_temp_user: User = g.user
-    if qq_temp_user.role >= 0:
+    if not qq_temp_user.is_temp:
         return jsonify({"msg": "User has already linked to an account."}), 410
-    account_to_link.
-    new_user: User = User(username=username,
-                          password=bcrypt.hashpw(password.encode(), bcrypt.gensalt()),
-                          nickname=nickname,
-                          created_at=datetime.datetime.now(),
-                          role=0,
-                          email=email,
-                          email_verified=True,
-                          phone=phone,
-                          phone_verified=False,
-                          valid_since=datetime.datetime.now()
-                          )
-    db.session.add(new_user)
+
+    account_to_link.qq = qq_temp_user.qq
+    account_to_link.group_id = qq_temp_user.group_id
+    account_to_link.nickname = qq_temp_user.nickname
+
+    qq_temp_user.personal_records.update({
+        PersonalRecord.last_modified: datetime.datetime.now(),
+        PersonalRecord.user_id: account_to_link.id
+    })
+
+    qq_temp_user.hang_on_trees.update({
+        HangOnTree.user_id: account_to_link.id
+    })
+
+    db.session.delete(qq_temp_user)
     db.session.commit()
-    db.session.refresh(new_user)
 
-    token = {
-        "sub": new_user.id,
-        "iss": Config.DOMAIN_NAME,
-        "aud": Config.FRONTEND_DOMAIN_NAME,
-        "iat": int(datetime.datetime.now().timestamp()),
-        "remember": True,
-        "type": "login_credential"
-    }
-
-    signed = sign(token)
-
-    user_data = generate_user_dict(user=new_user, for_oneself=True)
+    user_data = generate_user_dict(user=account_to_link, for_oneself=True)
 
     return jsonify({
         "msg": "Successful!",
-        "user": user_data,
-        "jwt": signed
+        "user": user_data
     }), 200
