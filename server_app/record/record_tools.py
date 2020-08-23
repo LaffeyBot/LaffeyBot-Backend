d6fdad5b47a8b44df6d57c4d803e6e8039fb8a11
zjdavid <zjdavid.2003@gmail.com>
2020-08-23 21:15:02 +0800

    改刀

--- server_app/record/record_tools.py
+++ server_app/record/record_tools.py
@@ -1,4 +1,5 @@
-from data.model import PersonalRecord, TeamRecord
+from data.model import PersonalRecord, TeamRecord, TeamBattleEpoch, db
+import datetime
 from config import Config
 
 
@@ -23,3 +24,18 @@ def subtract_damage_from_group(record: PersonalRecord, team_record: TeamRecord):
             team_record.current_boss_order = 1
             team_record.current_boss_gen += 1
         team_record.boss_remaining_health = Config.BOSS_HEALTH[team_record.current_boss_order-1]
+
+
+def make_new_team_record(group_id: int) -> TeamRecord:
+    current_epoch: TeamBattleEpoch = TeamBattleEpoch.query \
+        .order_by(TeamBattleEpoch.end_date.desc()).limit(1).first()
+    team_record = TeamRecord(detail_date=datetime.datetime.now(),
+                             epoch_id=current_epoch.id,
+                             group_id=group_id,
+                             current_boss_gen=1,
+                             current_boss_order=1,
+                             boss_remaining_health=Config.BOSS_HEALTH[0],
+                             last_modified=datetime.datetime.now())
+    db.session.add(team_record)
+    db.session.commit()
+    db.session.refresh(team_record)
