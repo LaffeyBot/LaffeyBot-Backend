d746d79464cb057a1f9ae758010b4bcd7380c626
zjdavid <zjdavid.2003@gmail.com>
2020-08-24 13:27:01 +0800

    Report

---
+++ server_app/generate_report/__init__.py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+
+generate_report_blueprint = Blueprint(
+    "generate_report_blueprint",
+    __name__,
+    url_prefix='/v1/generate_report'
+)
+
+from .auth import *
