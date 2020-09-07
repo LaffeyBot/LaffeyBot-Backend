04df669e509d6546e9d438213cf8f8c830dad88a
zjdavid <zjdavid.2003@gmail.com>
2020-09-07 15:13:15 +0800

    gunicorn

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,10 @@
 from app import app as application
+import logging
+
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    application.logger.handlers = gunicorn_logger.handlers
+    application.logger.setLevel(gunicorn_logger.level)
 
 if __name__ == "__main__":
     application.run()
